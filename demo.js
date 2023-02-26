42ff3a752b20af3116d591d5638b547047e1dedf
Chandraprakash-Patidar <chandu.patidar.88@gmail.com>
2023-02-26 17:28:24 +0530

    update index.html and demo.js

--- demo.js
+++ demo.js
@@ -1,1 +1,23 @@
-console.log("git demo")
+//console.log("git demo")
+// console.log("hello")
+// console.log("hello again")
+//console.dir(document)
+// console.log(document.domain)
+// console.log(document.URL)
+// console.log(document.title)
+// console.log(document.textContent)
+// console.log(document.doctype)
+// console.log(document.head)
+// console.log(document.all)
+console.log(document.getElementById('header-title'));
+var headerTitle =document.getElementById('header-title');
+//  console.log(headerTitle);
+// headerTitle.textContent='hello';
+// headerTitle.innerText='goodbye';
+var header =document.getElementById('main-header')
+console.log(document.getElementById('title1'))
+///headerTitle.style.borderBottom='solid 3px #000'
+header.style.borderBottom='solid 3px #000'
+var additem = document.getElementsByClassName('title')[0];
+additem.style.fontWeight='bold';
+additem.style.color='green'
